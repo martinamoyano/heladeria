cf121a194692f90b08332e09a2de59f37d56ac44
Martina Moyano <martina.moyano@gmail.com>
2023-12-01 17:43:42 -0300

    Guardar cambios en el directorio src

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ContactForm.css';
 
 function ContactForm() {
+     const [nombre, setNombre] = useState("");
+     const [email, setEmail] = useState("");
+     const [mensaje, setMensaje] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(nombre);
+        console.log(email);
+        console.log(mensaje);
+    }
+
     return (
         <div className="container">
             <h1>¡Contáctanos!</h1>
-            <form action="#" method="post">
+            <form onSubmit={handleSubmit} action="#" method="post">
                 <div className="form-group">
                     <label htmlFor="nombre">Nombre:</label>
-                    <input type="text" id="nombre" name="nombre" required />
+                    <input type="text" id="nombre" name="nombre" required value=
+                    {nombre} onChange = {(e) => setNombre(e.target.value)}/>
                 </div>
                 <div className="form-group">
                     <label htmlFor="correo">Correo Electrónico:</label>
-                    <input type="email" id="correo" name="correo" required />
+                    <input type="email" id="correo" name="correo" required value=
+                    {email} onChange = {(e) => setEmail(e.target.value)}/>
                 </div>
                 <div className="form-group">
                     <label htmlFor="mensaje">Mensaje:</label>
-                    <textarea id="mensaje" name="mensaje" required></textarea>
+                    <textarea id="mensaje" name="mensaje" required value=
+                    {mensaje} onChange = {(e) => setMensaje(e.target.value)}></textarea>
                 </div>
                 <div className="form-group">
                     <input type="submit" value="Enviar" />
@@ -25,5 +39,3 @@ function ContactForm() {
         </div>
     );
 }
-
-export default ContactForm;
