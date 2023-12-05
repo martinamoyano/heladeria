98709fa3a2c88a03dea1d0e8f3f1c59698ee1a6e
Martina Moyano <martina.moyano@gmail.com>
2023-12-05 18:05:03 -0300

    ahora anda el getAll :)

--- back/dbConfig.js
+++ back/dbConfig.js
@@ -1,10 +1,33 @@
 import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const dbConfig = {
   host: "localhost",
   user: "root",
   port: 3306,
-  database: "heladeria",
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 };
 
 export const connection = mysql.createPool(dbConfig);
+
+// import mysql from "mysql2/promise";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// const dbConfig = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   port: 3306,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// });
+
+// dbConfig.on('acquire', connection => {
+//   console.log('Conectado');
+// });
+
+// export default dbConfig;
--- back/index.js
+++ back/index.js
@@ -1,14 +1,14 @@
 import express from "express";
+import cors from "cors";
 import { router as heladeriaRt } from "./src/heladeria/heladeriaRt.js";
-  
-const app = express();
-const port = 3000; 
-  
-app.use(express.json());
 
+const app = express(); 
+const port = 3000; 
+
+app.use(cors()); 
+app.use(express.json());
 app.use("/heladeria", heladeriaRt);
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
 });
-  
